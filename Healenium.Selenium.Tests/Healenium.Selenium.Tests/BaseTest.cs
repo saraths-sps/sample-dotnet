50ac501f311354f2f901c94512a210b2c45ab95e
saraths-sps <112088198+saraths-sps@users.noreply.github.com>
2023-08-31 20:26:20 +0530

    Update BaseTest.cs

--- Healenium.Selenium.Tests/Healenium.Selenium.Tests/BaseTest.cs
+++ Healenium.Selenium.Tests/Healenium.Selenium.Tests/BaseTest.cs
@@ -18,24 +18,24 @@ namespace Healenium.Selenium.Tests
         [Obsolete]
         public static void SetUp()
         {
-            var browserOptions = new ChromeOptions();
-            browserOptions.PlatformName = "Windows 10";
-            browserOptions.BrowserVersion = "latest"
-            var sauceOptions = new Dictionary<string, object>();
-            sauceOptions.Add("username", "oauth-manisharajamani97-3541d");
-            sauceOptions.Add("accessKey", "e4fb9dca-9c93-4566-80ea-c254d12614e9");
-            sauceOptions.Add("build", "<your build id>");
-            sauceOptions.Add("name", "<your test name>");
-            browserOptions.AddAdditonalOption("sauce:options", sauceOptions);
+           // var browserOptions = new ChromeOptions();
+           // browserOptions.PlatformName = "Windows 10";
+           // browserOptions.BrowserVersion = "latest"
+           // var sauceOptions = new Dictionary<string, object>();
+            //sauceOptions.Add("username", "oauth-manisharajamani97-3541d");
+           // sauceOptions.Add("accessKey", "e4fb9dca-9c93-4566-80ea-c254d12614e9");
+           // sauceOptions.Add("build", "<your build id>");
+           // sauceOptions.Add("name", "<your test name>");
+           // browserOptions.AddAdditonalOption("sauce:options", sauceOptions);
 
  
 
-            var uri = new Uri("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
-            var driver = new RemoteWebDriver(uri, browserOptions);
+            //var uri = new Uri("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+            //var driver = new RemoteWebDriver(uri, browserOptions);
             
-            //var optionsChrome = new ChromeOptions();
-            //optionsChrome.AddArguments("--no-sandbox");
-            //_driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), optionsChrome);
+            var optionsChrome = new ChromeOptions();
+            optionsChrome.AddArguments("--no-sandbox");
+            _driver = new RemoteWebDriver(new Uri("http://3.80.249.158:4444/wd/hub"), optionsChrome);
 
             //var options = new FirefoxOptions();
             //_driver = new RemoteWebDriver(new Uri("http://localhost:8085"), options);
